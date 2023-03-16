b3f96150527c2b10e555f95c63742dc6b2aeeb4f
servicefloww <servicefloww@gmail.com>
2023-03-16 13:58:05 +0530

    codevibe

---
+++ app.js
@@ -0,0 +1,6 @@
+function menu(){
+    navbar = document.getElementById("navbar")
+    navbar.classList.toggle("active")
+    hide = document.getElementById("hide")
+    hide.classList.toggle("show")
+}
