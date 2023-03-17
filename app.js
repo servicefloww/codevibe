a6406a8730ce000730b0c7975351c932a9284e99
servicefloww <servicefloww@gmail.com>
2023-03-17 10:15:17 +0530

    Learning page

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 function menu(){
     navbar = document.getElementById("navbar")
     navbar.classList.toggle("active")
-    hide = document.getElementById("hide")
+    hide = document.getElementById("right")
+    hide.classList.toggle("hide")
+    about = document.getElementById("hide")
     hide.classList.toggle("show")
 }
---
+++ learn.js
@@ -0,0 +1,6 @@
+function navfunc(){
+    navBar = document.getElementById("navbar")
+    navBar.classList.toggle("active")
+    hide = document.getElementById("hide")
+    hide.classList.toggle("show")
+}
